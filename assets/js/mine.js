e6dd8850b7baae4fe18ec950c49b927c4c6caf15
OutrageGames <outragegames@hotmail.com>
2023-02-27 21:15:25 +0200

    new site lol

---
+++ assets/js/mine.js
@@ -0,0 +1,22 @@
+function click (e) {
+  if (!e)
+    e = window.event;
+  if ((e.type && e.type == "contextmenu") || (e.button && e.button == 2) || (e.which && e.which == 3)) {
+    if (window.opera)
+      window.alert("");
+    return false;
+  }
+}
+if (document.layers)
+  document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown = click;
+document.oncontextmenu = click;
+
+
+window.addEventListener("dragover",function(e){
+  e = e || event;
+  e.preventDefault();
+},false);
+window.addEventListener("drop",function(e){
+  e = e || event;
+  e.preventDefault();},false);
